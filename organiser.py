e0d0ef6c604e8d935802c9b78d0ac05ab2eb9e44
Daniel <danielsamet98@gmail.com>
2018-06-14 14:42:45 +0100

    Improved printing. Added show_all_tables function. Fixed purge_data function.

--- organiser.py
+++ organiser.py
@@ -1,15 +1,31 @@
 import sqlite3
+import time
 
 
 class Organiser:
     """quick db functionality (mainly for testing purposes) before GUI is built"""
 
-    def db_opener(self, database_name, foreign_keys=True):
+    def _db_opener(self, database_name, foreign_keys=True):
+        """internal use only - opens connections to database"""
+
         connection = sqlite3.connect(database_name)
         cursor = connection.cursor()
         if foreign_keys:
             cursor.execute("PRAGMA foreign_keys = 1")
         return connection, cursor
+
+    def _db_execute(self, sql_command, *parameters):
+        """internal use only - executes commands on database and returns results"""
+
+        connection, cursor = self._db_opener("tuck.db")
+
+        cursor.execute(sql_command, *parameters)
+        connection.commit()
+        results = cursor.fetchall()
+
+        cursor.close(), connection.close()
+
+        return results
 
     def create_database(self):
         """initialises the database with all necessary tables with correct structure"""
@@ -156,58 +172,63 @@ class Organiser:
             
             """)
 
-        connection, cursor = self.db_opener("tuck.db", )
-
         try:
             for command in sql_command:
-                cursor.execute(command)
-            connection.commit()
+                self._db_execute(command)
         except sqlite3.OperationalError:  # tables already exist (no need to worry abt one existing and not another)
-            print("table already exists")
-
-        cursor.close(), connection.close()
+            print("table already exists\n")
 
     def show_database_structure(self):
         """returns current database structure"""
 
-        connection, cursor = self.db_opener("tuck.db")
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = [item[0] for item in cursor.fetchall()]
-        
+        tables = [item[0] for item in self._db_execute("SELECT name FROM sqlite_master WHERE type='table';")]
+
         for table in tables:
-            print(table)
-            cursor.execute("PRAGMA TABLE_INFO({})".format(table))
-            [print("\t", column) for column in cursor.fetchall()]
-            
-        cursor.close(), connection.close()
+            print("Showing table structure for table \"{0}\":".format(table))
+            [print("\t", column) for column in self._db_execute("PRAGMA TABLE_INFO({})".format(table))]
+
+        print("")
 
     def show_table(self, table_name):
         """returns table of data (using table_name)"""
 
-        connection, cursor = self.db_opener("tuck.db")
+        print("Records found in table \"{0}\":".format(table_name))
 
-        cursor.execute("SELECT * FROM {}".format(table_name))
-        print(cursor.fetchall())
+        table = self._db_execute("SELECT * FROM {}".format(table_name))
 
-        cursor.close(), connection.close()
+        [print(table) for table in table]
+        if not table:
+            print("--No Records in Table--")
+        print("")
+
+    def show_all_tables(self):
+        """returns records for all tables"""
+
+        tables = [item[0] for item in self._db_execute("SELECT name FROM sqlite_master WHERE type='table';")]
+        [self.show_table(table) for table in tables]
 
     def purge_data(self):
         """purge database of all data leaving just table structure"""
 
-        connection, cursor = self.db_opener("tuck.db")
+        tables = [item[0] for item in self._db_execute("SELECT name FROM sqlite_master WHERE type='table';")]
 
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = [item[0] for item in cursor.fetchall()]
-        [cursor.execute("DROP TABLE {}".format(table)) for table in tables]
-        connection.commit()
-
-        cursor.close(), connection.close()
+        temp = int()
+        while temp < len(tables):
+            for table in tables:
+                try:
+                    try:
+                        self._db_execute("DROP TABLE {}".format(table))
+                        temp += 1
+                    except sqlite3.OperationalError:  # table has already been dropped
+                        continue
+                except sqlite3.IntegrityError:
+                    time.sleep(0.01)
 
         self.create_database()
 
 
 if __name__ == "__main__":
-    Organiser().create_database()
+    # Organiser().create_database()
     # Organiser().show_database_structure()
-    Organiser().show_table("accounts")
-    # Organiser().purge_data()
+    Organiser().show_all_tables()
+    Organiser().purge_data()
